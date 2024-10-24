b0cceed975baf92d30f4538b219b94e040c40fec
josselinonduty <contact@josselinonduty.fr>
2024-10-24 12:56:33 +0200

    feat: add matrix operations and use matrix.length instead of matrix.size (unset memory)

--- include/functions.h
+++ include/functions.h
@@ -4,19 +4,25 @@
 #define C (long double) 0.4
 #define N (long unsigned int) 2
 
-typedef size_t step_t;
+typedef long double step_t;
 typedef long double *vector_t;
 struct __matrix_t {
     vector_t *storage;
     size_t size;
+    size_t length;
 };
 typedef struct __matrix_t matrix_t;
 
 void vector_display(const vector_t vector);
 void matrix_display(const matrix_t matrix);
+void matrix_save_csv(const char *filename, const matrix_t matrix);
 
 vector_t vector_create(vector_t vector);
 void vector_free(vector_t *vector);
+
+matrix_t matrix_create(size_t n);
+void matrix_append(matrix_t *matrix, vector_t vector);
+void matrix_free(matrix_t *matrix);
 
 vector_t van_der_pol(step_t t, vector_t prev);
 
--- src/functions.c
+++ src/functions.c
@@ -10,10 +10,24 @@ void vector_display(const vector_t vector)
 
 void matrix_display(const matrix_t matrix)
 {
-	printf("%lix%li", N, matrix.size);
-	for (size_t i = 0; i < matrix.size; i++) {
+	printf("matrix_t(%li, %li)\n", N, matrix.length);
+
+	for (size_t i = 0; i < matrix.length; i++) {
 		vector_display(matrix.storage[i]);
+		printf("\n");
 	}
+}
+
+void matrix_save_csv(const char *filename, const matrix_t matrix)
+{
+	FILE *file = fopen(filename, "w");
+
+	for (size_t i = 0; i < matrix.length; i++) {
+		fprintf(file, "%ld\t%Lf\t%Lf\n", i, matrix.storage[i][0],
+			matrix.storage[i][1]);
+	}
+
+	fclose(file);
 }
 
 vector_t vector_create(vector_t vector)
@@ -31,6 +45,38 @@ void vector_free(vector_t *vector)
 {
 	free(*vector);
 	*vector = NULL;
+}
+
+matrix_t matrix_create(size_t n)
+{
+	matrix_t matrix = {
+		.size = n,
+		.length = 0,
+		.storage = (vector_t *) malloc(n * sizeof(vector_t))
+	};
+
+	for (long int i = 0; i < matrix.size; i++) {
+		matrix.storage[i] = NULL;
+	}
+
+	return matrix;
+}
+
+void matrix_append(matrix_t *matrix, vector_t vector)
+{
+	matrix->storage[matrix->length++] = vector;
+}
+
+void matrix_free(matrix_t *matrix)
+{
+	for (long int i = 0; i < matrix->length; i++) {
+		vector_free(&(matrix->storage[i]));
+	}
+
+	matrix->size = 0;
+	matrix->length = 0;
+	free(matrix->storage);
+	matrix->storage = NULL;
 }
 
 vector_t van_der_pol(step_t t, vector_t prev)
